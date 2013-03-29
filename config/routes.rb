77e3d66d12dfa034cbd5f786de2d63e8251801d0
RW <rwu1985@gmail.com>
2013-03-29 00:25:44 -0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Raytest::Application.routes.draw do
+
+  root :to => 'static_pages#home'
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/contact"
+  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
