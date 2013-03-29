ac84b8188d5a9861691d1f350aaa4f7ff3041481
RW <rwu1985@gmail.com>
2013-03-29 00:03:48 -0700

    Fix gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raytest::Application.config.secret_token = '151893bff66abc4496948ddf57c0ce95967391ac3353167e4af2a9d7cbfa944ea4565314d9499501c481c3f86a86337ebe53ec3f3805d0af29d4d414381e21e5'
