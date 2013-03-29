29bd3399d800c55b381aa2587a521712b8a43861
RW <rwu1985@gmail.com>
2013-03-29 00:17:18 -0700

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Raytest::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
